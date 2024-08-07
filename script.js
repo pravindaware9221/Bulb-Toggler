14175cd731377b771646187077e24c3fe84c8d1e
Pravin <pravindaware9221@gmail.com>
2024-08-07 11:23:05 +0530

    add all file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function BulbOFF() {
+    const BulbOFF = document.getElementById("imG")
+    BulbOFF.src = "./off.png";
+}
+function BulbON() {
+    const BulbON = document.getElementById("imG")
+    BulbON.src = "./on.png";
+}
